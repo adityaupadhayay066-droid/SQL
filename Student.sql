106b72f32da18a0411927fc62b2669d0fa8af3ef
adityaupadhayay066-droid <adityaupadhayay066@gmail.com>
2026-02-03 22:30:38 +0530

    Students detail

---
+++ Student.sql
@@ -0,0 +1,16 @@
+use employee;
+CREATE TABLE STUDENTS(
+student_id INT PRIMARY KEY,
+name VARCHAR(50),
+Department VARCHAR(50),
+Age INT,
+M_name varchar(50)
+);
+INSERT INTO STUDENTS Values(
+019, "Abhinav", "D series",18, "Nikita singh"),
+(256, "Sunny sharna", "BPHARMA",18, "Anjali sharma"
+);
+drop table students;
+
+SELECT * FROM STUDENTS;
+desc Students;
